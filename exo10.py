d795c15ae3d8ebfe215e5e40d3971a63b7ae5323
NGhislain <nathanghislain2801@gmail.com>
2021-12-23 10:04:21 +0100

    Ajout exo 10

---
+++ exo10.py
@@ -0,0 +1,12 @@
+number = input("Entre un nombre ")
+
+hour = int(number) / 3600
+
+minute = int(number) / 60
+
+
+print("En heure ça donne " + str(hour) )
+
+print("En minute ça donne " + str(minute))
+
+print("En seconde ça donne " + number)
